f8ad505072c22ed69f3712a3545f789c78a83d8d
Panagiotis Syskakis <panagiss@ece.auth.gr>
2023-11-24 12:49:25 +0200

    Remove node_modules folder from repo

--- node_modules/oas3-tools/dist/middleware/swagger.ui.options.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class SwaggerUiOptions {
-    apiDocsPath: string;
-    swaggerUIPath: string;
-    swaggerUiDir: string;
-    constructor(apiDocsPath: string, swaggerUIPath: string, swaggerUiDir: string);
-    private sanitize;
-}
