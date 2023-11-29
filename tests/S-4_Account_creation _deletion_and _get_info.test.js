b2f55cf3d24094373ae7c4377e17e74e18446a3d
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-11-29 16:47:43 +0200

    Auto format code

--- tests/S-4_Account_creation _deletion_and _get_info.test.js
+++ tests/S-4_Account_creation _deletion_and _get_info.test.js
@@ -82,11 +82,11 @@ test("Multiple POST endpoint /admin/user", async (t) => {
     ];
 
     for (const userData of usersToCreate) {
-        const { statusCode } = await t.context.got.post("admin/user", {json: userData});
+        const { statusCode } = await t.context.got.post("admin/user", { json: userData });
 
         // Check status Code 
         t.is(statusCode, 200, `Expected status code 200 for successful deletion, Bad request for user ${userData.userName}`);
-       
+
     }
 });
 
@@ -94,14 +94,14 @@ test("Multiple POST endpoint /admin/user", async (t) => {
 // Test for Wrong input:    POST    /admin/user
 test("Wrong input POST endpoint /admin/user", async (t) => {
 
-    const { statusCode } = await t.context.got.post("admin/user", { 
+    const { statusCode } = await t.context.got.post("admin/user", {
         // Give it dummy data 
         json: {
-        //     surname: "surname_dummy",
-        //     name: "name_dummy",
-        //     id: 0,
-        //     userName: "userName_dummy",
-        //     email: "email@dummy_data"
+            //     surname: "surname_dummy",
+            //     name: "name_dummy",
+            //     id: 0,
+            //     userName: "userName_dummy",
+            //     email: "email@dummy_data"
         }
     });
 
@@ -116,7 +116,7 @@ test("Empty input POST endpoint /admin/user", async (t) => {
 
     await t.throwsAsync(
         async () => {
-            await t.context.got.post("admin/user", { })
+            await t.context.got.post("admin/user", {})
         },
         { instanceOf: t.context.got.HTTPError, message: /Response code 415/ }
         // Or we can use this 'Response code 415 (Unsupported Media Type)'        
@@ -137,12 +137,12 @@ test("Empty input POST endpoint /admin/user", async (t) => {
 // Test for:    DELETE  /admin/user/{userName}
 test("DELETE endpoint /admin/user/{userName}", async (t) => {
 
-    const userNameToDelete = "dummyUserName";     
+    const userNameToDelete = "dummyUserName";
     const { statusCode } = await t.context.got.delete(`admin/user/${userNameToDelete}`);
 
     // Check status Code 
     t.is(statusCode, 200, "Expected status code 200 for successful deletion");
-   
+
 });
 
 // Test for mulpuple: DELETE  /admin/user/{userName}
@@ -155,7 +155,7 @@ test("Multiple DELETE endpoint /admin/user/{userName}", async (t) => {
         // Check status Code 
         t.is(statusCode, 200, `Expected status code 200 for successful deletion, Bad request for user ${userNameToDelete}`);
     }
-    
+
 });
 
 // Test for Empty input:    DELETE  /admin/user/{userName}
@@ -173,38 +173,38 @@ test("Empty input DELETE endpoint /admin/user/{userName}", async (t) => {
 
 // =========================== GET /user/{userName}/class/{className} =========================== //
 
-const testValidClass = (t, body , className ) => {
-    
+const testValidClass = (t, body, className) => {
+
     // Make sure it has a className and it is the expected one
     t.not(body.className, undefined, "Response should have a className property");
     t.is(body.className, className, `Expected className to be equal with that of the request ${className}`);
-    
+
     // Make sure it is an array and that it has a length
     t.true(Array.isArray(body.users), "Users should be an array");
-    t.not(body.users.length,undefined, "Expected to be able to get lenght of users array");
-    
+    t.not(body.users.length, undefined, "Expected to be able to get lenght of users array");
+
     // Loop over all users in the array
-    for (const user of body.users){
+    for (const user of body.users) {
         // Check that the struct has the proper fields  
-        testValidUser(t , user );                    
+        testValidUser(t, user);
     }
 }
 
-const testValidUser = (t , dummy_user) => {        
+const testValidUser = (t, dummy_user) => {
     // Check that the struct has the proper fields  
-    t.not(dummy_user.grade,          undefined, "User should have a grade property");
-    t.not(dummy_user.user,           undefined, "User should have a user property");
-    t.not(dummy_user.user.userName,  undefined, "User should have a userName property");
-    t.not(dummy_user.user.surname,   undefined, "User should have a surname property");
-    t.not(dummy_user.user.name,      undefined, "User should have a name property");
-    t.not(dummy_user.user.id,        undefined, "User should have a id property");
-    t.not(dummy_user.user.email,     undefined, "User should have a email property");
+    t.not(dummy_user.grade, undefined, "User should have a grade property");
+    t.not(dummy_user.user, undefined, "User should have a user property");
+    t.not(dummy_user.user.userName, undefined, "User should have a userName property");
+    t.not(dummy_user.user.surname, undefined, "User should have a surname property");
+    t.not(dummy_user.user.name, undefined, "User should have a name property");
+    t.not(dummy_user.user.id, undefined, "User should have a id property");
+    t.not(dummy_user.user.email, undefined, "User should have a email property");
 
 }
 
 // Test for:    GET     /user/{userName}/class/{className}
 test("GET endpoint /user/{userName}/class/{className}", async (t) => {
-    const userName = "userName"; 
+    const userName = "userName";
     const className = "className"; // Don't change that because the dummy data returns only for this className
 
     const { body, statusCode } = await t.context.got.get(`user/${userName}/class/${className}`);
@@ -221,57 +221,57 @@ test("Multiple usernames GET endpoint /user/{userName}/class/{className}", async
     const usernames = ["userName", "userName2", "userName3", "userName4", "userName5", "userName6"];
     const className = "className"; // Don't change that because the dummy data returns only for this className
 
-    
+
     // Pre-allocate the array based on the number of usernames
     const responses = new Array(usernames.length);
-    
+
     // Iterate over each username
     for (let i = 0; i < usernames.length; i++) {
         // Make the GET request
         const response = await t.context.got.get(`user/${usernames[i]}/class/${className}`);
-        
+
         // Check status Code
         t.is(response.statusCode, 200, `Expected status code 200 for user: ${usernames[i]}`);
-        
+
         // Assign the response to the pre-allocated array
         responses[i] = response;
     }
-    
-    testValidClass(t, responses[0].body , className);
-    
+
+    testValidClass(t, responses[0].body, className);
+
     // Check that all responses have the same body
     for (let i = 1; i < responses.length; i++) {
         t.deepEqual(
             responses[i].body,
             responses[0].body,
             `Expected the same body response for ${usernames[i]} as for ${usernames[0]}`
-            );
-        }
-        
+        );
+    }
+
 });
 
 // Test for Multiple usernames and classNames:    GET     /user/{userName}/class/{className}
 test("Multiple usernames and classNames GET endpoint /user/{userName}/class/{className}", async (t) => {
     const usernames = ["userName1", "userName2", "userName3"];
     const classNames = ["className1", "className2", "className3"];
-    
+
     // Iterate over each className
     for (const className of classNames) {
         // Iterate over each username
-        var expected_response ;
+        var expected_response;
         for (const userName of usernames) {
             // Make the GET request
             const response = await t.context.got.get(`user/${userName}/class/${className}`);
-            
+
             // Check status Code
-            t.is(response.statusCode, 200, `Expected status code 200 for ${userName} and ${className}`);             
-            
+            t.is(response.statusCode, 200, `Expected status code 200 for ${userName} and ${className}`);
+
             // Check Response body once for each className (since we check the other later against thoses ones)
-            if (userName == usernames[0]){
-                expected_response = response ; 
+            if (userName == usernames[0]) {
+                expected_response = response;
                 testValidClass(t, response.body, className);
             }
-            else{
+            else {
                 t.deepEqual(
                     response.body,
                     expected_response.body,
@@ -286,41 +286,41 @@ test("Multiple usernames and classNames GET endpoint /user/{userName}/class/{cla
 
 // Test for Empty className "input":    GET     /user/{userName}/class/{className}
 test("Empty className GET endpoint /user/{userName}/class/{className}", async (t) => {
-    const userName = "userName"; 
+    const userName = "userName";
     const className = ""; // Don't change that because the dummy data returns only for this className
     await t.throwsAsync(
         async () => {
             await t.context.got.get(`user/${userName}/class/${className}`);
         },
         { instanceOf: t.context.got.HTTPError, message: /Response code 404/ }
-    );   
-    
+    );
+
 });
-    
+
 // Test for Empty userName "input":    GET     /user/{userName}/class/{className}
 test("Empty userName GET endpoint /user/{userName}/class/{className}", async (t) => {
-    const userName = ""; 
-    const className = "className"; 
+    const userName = "";
+    const className = "className";
 
     await t.throwsAsync(
         async () => {
             await t.context.got.get(`user/${userName}/class/${className}`);
         },
         { instanceOf: t.context.got.HTTPError, message: /Response code 404/ }
-    );   
-    
+    );
+
 });
 
 // Test for Empty userName and className "input":    GET     /user/{userName}/class/{className}
 test("Empty both GET endpoint /user/{userName}/class/{className}", async (t) => {
-    const userName = ""; 
-    const className = ""; 
-    
+    const userName = "";
+    const className = "";
+
     await t.throwsAsync(
         async () => {
             await t.context.got.get(`user/${userName}/class/${className}`);
         },
         { instanceOf: t.context.got.HTTPError, message: /Response code 404/ }
-    );   
-    
+    );
+
 });
