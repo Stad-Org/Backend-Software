c91e97a4250cf53e52eb77e15c072dafbf8dd8c6
deepblue597 <iasonas2001@hotmail.com>
2023-12-03 18:34:16 +0200

    get info class by admin test

--- tests/S-3_Create_Get_Delete_Class.test.js
+++ tests/S-3_Create_Get_Delete_Class.test.js
@@ -1,33 +1,52 @@
-const test = require('ava')
-const app = require('../index.js');
-const http = require('http');
-const listen = require('test-listen');
-const got = require('got');
+const test = require("ava");
+const app = require("../index.js");
+const http = require("http");
+const listen = require("test-listen");
+const got = require("got");
 
 // Initialize server
 test.before(async (t) => {
-    console.log("Creating test server")
-    t.context.server = http.createServer(app);
-    t.context.prefixUrl = await listen(t.context.server);
-    t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json' });
-    console.log("Successfully created test server")
-})
+  console.log("Creating test server");
+  t.context.server = http.createServer(app);
+  t.context.prefixUrl = await listen(t.context.server);
+  t.context.got = got.extend({
+    prefixUrl: t.context.prefixUrl,
+    responseType: "json",
+  });
+  console.log("Successfully created test server");
+});
 
 // Shutdown server
 test.after.always(async (t) => {
-    console.log("Testing finished, closing test server")
-    await t.context.server.close();
-    console.log("Successfully closed test server")
+  console.log("Testing finished, closing test server");
+  await t.context.server.close();
+  console.log("Successfully closed test server");
+});
 
-})
+// // Replace this with correct endpoints
+// test("endpoint User", async (t)=>{
+//     const {body, statusCode} = await t.context.got("user/123");
+//     t.is(statusCode, 200);
+//     t.is(body.surname, 'surname');
+//     t.is(body.name, 'name');
+//     t.is(body.id, 0);
+//     t.is(body.userName, 'userName');
+//     t.is(body.email, 'email');
+// })
 
-// Replace this with correct endpoints
-test("endpoint User", async (t)=>{
-    const {body, statusCode} = await t.context.got("user/123");
-    t.is(statusCode, 200);
-    t.is(body.surname, 'surname');
-    t.is(body.name, 'name');
-    t.is(body.id, 0);
-    t.is(body.userName, 'userName');
-    t.is(body.email, 'email');
-})
+/**
+ * Get admin class info
+ */
+test("GET /admin/class/{className} returns the data of the class", async (t) => {
+  const { statusCode, body } = await t.context.got("admin/class/className");
+
+  t.plan(6);
+  t.is(statusCode, 200);
+  console.log(body);
+  t.is(body.className, "className");
+  t.is(body.users[0].grade, 6.027456183070403);
+  t.is(body.users[1].user.id, 0);
+
+  t.not(body.users[1].user.name, "Jason");
+  t.not(body.users[0].grade, 12);
+});
--- tests/examples.test.js
+++ tests/examples.test.js
@@ -1,72 +1,96 @@
 // Example file for writing tests
 
-const test = require('ava')
+const test = require("ava");
 
-test('my test', t => {
-    t.pass();
-    // t.fail();
-})
+/**
+ * Simple pass or fail test
+ */
+test("my test", (t) => {
+  t.pass();
+  // t.fail();
+});
 
-function addNumbers(a, b){
-    if (!(typeof a === "number" && typeof b === "number")){
-        throw new Error("Bad input types")
-    }
-    return a + b
+test("Simple addition", (t) => {
+  const num = 2;
+  t.is(num + 1, 3);
+});
+
+/**
+ *
+ * @param {Number} a first number of addition
+ * @param {Number} b seconf number of addition
+ * @returns sumof the 2 numbers
+ */
+function addNumbers(a, b) {
+  if (!(typeof a === "number" && typeof b === "number")) {
+    throw new Error("Bad input types");
+  }
+  return a + b;
 }
 
-test("addNumbers", t => {
-    t.is(addNumbers(1,2), 3);
-    t.throws(() => addNumbers("one", "two"));
-})
+test("addNumbers", (t) => {
+  t.plan(2); // How many tests i plan to make
 
-test("Async", async t => {
-    const res = Promise.resolve('test');
-    t.is(await res, 'test')
-})
+  t.is(addNumbers(1, 2), 3);
+  t.throws(() => addNumbers("one", "two"));
+});
+
+// test("Async", async (t) => {
+//   const res = Promise.resolve("test");
+//   t.is(await res, "test");
+// });
 
 // API function testing
-const {getUserInfo} = require('../service/DefaultService.js');
+// const { getUserInfo } = require("../service/DefaultService.js");
 
-test("getUserInfo", async t => {
-    const res = await getUserInfo();
-    t.is(res.surname, 'surname');
-    t.is(res.name, 'name');
-    t.is(res.id, 0);
-    t.is(res.userName, 'userName');
-    t.is(res.email, 'email');
-
-})
+// test("getUserInfo", async (t) => {
+//   const res = await getUserInfo();
+//   t.is(res.surname, "surname");
+//   t.is(res.name, "name");
+//   t.is(res.id, 0);
+//   t.is(res.userName, "userName");
+//   t.is(res.email, "email");
+// });
 
 // Server testing
 
-const app = require('../index.js');
-const http = require('http');
-const listen = require('test-listen');
-const got = require('got');
+const app = require("../index.js"); // the app with the routes
+const http = require("http"); // take the express app and go live as a server
+const listen = require("test-listen"); // create a test server to send requests
+const got = require("got"); // for requests
 
+/**
+ * before the test of the files run this function is running
+ */
 test.before(async (t) => {
-    console.log("Creating test server")
-    t.context.server = http.createServer(app);
-    t.context.prefixUrl = await listen(t.context.server);
-    t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json' });
-    console.log("Successfully created test server")
-})
+  console.log("Creating test server");
+  t.context.server = http.createServer(app); // I can save things here. I create the server
+  t.context.prefixUrl = await listen(t.context.server); // Where to send the requests. Async function
+  t.context.got = got.extend({
+    //change default settings
+    prefixUrl: t.context.prefixUrl, //the url where we send requests
+    responseType: "json",
+  });
+  console.log("Successfully created test server");
+});
 
+/**
+ * After all the tests have finished
+ */
 test.after.always(async (t) => {
-    console.log("Testing finished, closing test server")
-    await t.context.server.close();
-    console.log("Successfully closed test server")
+  console.log("Testing finished, closing test server");
+  await t.context.server.close();
+  console.log("Successfully closed test server");
+});
 
-})
-
-test("endpoint User", async (t)=>{
-    const {body, statusCode} = await t.context.got("user/123");
-    t.is(statusCode, 200);
-    t.is(body.surname, 'surname');
-    t.is(body.name, 'name');
-    t.is(body.id, 0);
-    t.is(body.userName, 'userName');
-    t.is(body.email, 'email');
-})
+// test("endpoint User", async (t) => {
+//   const { body, statusCode } = await t.context.got("user/123");
+//   t.is(statusCode, 200);
+//   t.is(body.surname, "surname");
+//   t.is(body.name, "name");
+//   t.is(body.id, 0);
+//   t.is(body.userName, "userName");
+//   t.is(body.email, "email");
+// });
 
 //Functions for API calls
