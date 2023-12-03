e2624c434553fb31dd6c11be78a05e90cbdfd445
Nantina <nbosinaki@gmail.com>
2023-12-03 19:34:57 +0200

    added test for wrong input

--- tests/S-2_Upload_Grades.test.js
+++ tests/S-2_Upload_Grades.test.js
@@ -7,12 +7,7 @@ const app = require('../index.js')
 const http = require('http')
 const listen = require('test-listen')
 const got = require('got')
-
-// This is needed to test each function individually 
 const onlyFunc = require('../service/DefaultService.js');
-// onlyFunc.createUser()
-// onlyFunc.deleteUser()
-// onlyFunc.getClassInfoUser
 
 // Initialize server
 test.before(async (t) => {
@@ -30,8 +25,6 @@ test.after.always(async (t) => {
   console.log('Successfully closed test server')
 })
 
-// =========================== POST /admin/class/{className}/user/{userName}/grade =========================== //
-
 // Test the function "uploadUserGrade" that gets called when the endpoint is used
 test('uploadUserGrade resolves when called with a grade Model', async (t) => {
   // Prepare data
@@ -40,7 +33,6 @@ test('uploadUserGrade resolves when called with a grade Model', async (t) => {
     className: 'class_name',
     username: 'user_name'
   };
-
   // Check that the function does NOT throw an error
   await t.notThrowsAsync(async () => {
     await onlyFunc.uploadUserGrade(gradeModel, gradeModel.username, gradeModel.className);
@@ -48,24 +40,21 @@ test('uploadUserGrade resolves when called with a grade Model', async (t) => {
 
 });
 
-// Test for:    POST    /admin/class/{className}/user/{userName}/grade
+// Test single POST
 test('POST endpoint /admin/class/{className}/user/{userName}/grade', async (t) => {
   const { statusCode } = await t.context.got.post('admin/class/{className}/user/{userName}/grade', {
-    // Give it dummy data
     json: {
         grade: 0.8008281904610115,
         className: 'class_name',
         username: 'user_name'
     }
   })
-
   // Check status Code
   t.is(statusCode, 200, 'Expected status code 200 for successful post')
 })
 
-// Test for Multiple:    POST    /admin/class/{className}/user/{userName}/grade
+// Test for Multiple POSTs
 test('Multiple POST endpoint /admin/class/{className}/user/{userName}/grade', async (t) => {
-  // Prepare data
   const gradesToUpload = [
     {
         grade: 5.534,
@@ -92,27 +81,30 @@ test('Multiple POST endpoint /admin/class/{className}/user/{userName}/grade', as
   }
 })
 
-// Test for Wrong input:    POST    /admin/class/{className}/user/{userName}/grade
+//Test for Wrong input
 test('Wrong input POST endpoint /admin/class/{className}/user/{userName}/grade', async (t) => {
-  const { statusCode } = await t.context.got.post('admin/class/{className}/user/{userName}/grade', {
-    json: {
-        grade: 0.8008281904610115,
-        className: 'class_name',
-        username: 'user_name'
+    try {
+      const { statusCode } = await t.context.got.post('admin/class/{className}/user/{userName}/grade', {
+        json: {
+          grade: 0.8008281904610115,
+          className: 796,
+          username: 'user_name'
+        }
+      });
+      // If the request succeeds (status code 200), fail the test
+      t.fail(`Expected request to fail with 400 status code, but received ${statusCode}`);
+    } catch (error) {
+      // Check status Code
+      t.is(error.response.statusCode, 400, 'Expected status code 400 for wrong input');
     }
   })
-
-  // Check status Code
-  t.is(statusCode, 200, 'Expected status code 200 for successful post')
-})
-
-// Test for Empty input:    POST    /admin/class/{className}/user/{userName}/grade
+  
+// Test for Empty input
 test('Empty input POST endpoint /admin/class/{className}/user/{userName}/grade', async (t) => {
   await t.throwsAsync(
     async () => {
       await t.context.got.post('admin/class/{className}/user/{userName}/grade', {})
     },
     { instanceOf: t.context.got.HTTPError, message: /Response code 415/ }
-    // Or we can use this 'Response code 415 (Unsupported Media Type)'
   )
 })
