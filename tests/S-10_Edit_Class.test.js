a583c6824a0bfea145f934c880694e86ce81632d
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-11 18:57:46 +0200

    Use actual Class object for testing PUT

--- tests/S-10_Edit_Class.test.js
+++ tests/S-10_Edit_Class.test.js
@@ -20,69 +20,90 @@ test.after.always(async (t) => {
     console.log("Testing finished, closing test server")
     await t.context.server.close();
     console.log("Successfully closed test server")
-
 })
+
+/**
+ * Class object to be used in tests
+ */
+const exampleClassObject = {
+    className: "maths",
+    users: [
+        {
+            grade: 6.027456183070403,
+            user: {
+                surname: "surname",
+                name: "name",
+                id: 0,
+                userName: "userName",
+                email: "email",
+            },
+        },
+        {
+            grade: 6.027456183070403,
+            user: {
+                surname: "surname",
+                name: "name",
+                id: 1,
+                userName: "userName",
+                email: "email",
+            },
+        },
+    ],
+};
 
 // ------------------ Test putClassInfoAdmin & PUT /admin/class/{className} ------------------
 
 // Test service function
 test("putClassInfoAdmin", async (t) => {
-    // The swaggerhub-generated backend doesn't do anything with the request
-    // body, so we cannot truly test it. We may leave it empty for now.
-    const requestBody = {
-        json: {}
-    }
+    const classroom = exampleClassObject
+    // The swaggerhub-generated backend doesn't do anything, so we can only
+    // test that it doesn't throw
     await t.notThrowsAsync(
-        DefaultService.putClassInfoAdmin(requestBody, "className")
+        DefaultService.putClassInfoAdmin({json: classroom}, "className")
     )
 })
 
 // Test endpoint for correct input
 test("PUT /admin/class/{className}", async (t) => {
-    const requestBody = {
-        json: {}
-    }
+    // Ideally would test for multiple different Class objects, but 
+    // no point doing that since the backend doesn't do anything anyway
+    const classroom = exampleClassObject
     // Test a bunch of valid requests
     const classNames = ["someClass", "someOtherClass"]
     for (const className of classNames) {
-        const { statusCode } = await t.context.got.put(`admin/class/${className}`, requestBody);
+        const { statusCode } = await t.context.got.put(`admin/class/${className}`, {json: classroom});
         t.is(statusCode, 200);
     }
 })
 
-// Test endpoint for incorrect input
+// Test endpoint for empty className
 test("Wrong input PUT /admin/class/{className}", async (t) => {
-    // Test empty className throws
-    const requestBody = {
-        json: {}
-    }
-    await t.throwsAsync(t.context.got.put(`admin/class/`, requestBody))
+    const classroom = exampleClassObject
+    await t.throwsAsync(t.context.got.put(`admin/class/`, {json: classroom}))
 })
 
 // ------------------ Test putClassInfoUser & PUT /user/{userName}/class/{className} ------------------
 
 // Test service function
 test("putClassInfoUser", async (t) => {
-    const requestBody = {
-        json: {}
-    }
+    const classroom = exampleClassObject
     await t.notThrowsAsync(
-        DefaultService.putClassInfoUser(requestBody, "userName", "className")
+        DefaultService.putClassInfoUser({json: classroom}, "userName", "className")
     )
 
 })
 
 // Test endpoint for correct input
 test("PUT /user/{userName}/class/{className}", async (t) => {
-    const requestBody = {
-        json: {}
-    }
+    // Ideally would test for multiple different Class objects, but 
+    // no point doing that since the backend doesn't do anything anyway
+    const classroom = exampleClassObject
     // Test a bunch of valid requests 
     const classNames = ["someClass", "someOtherClass"]
     const userNames = ["someUser", "someOtherUser"]
     for (const className of classNames) {
         for (const userName of userNames) {
-            const { statusCode } = await t.context.got.put(`user/${userName}/class/${className}`, requestBody);
+            const { statusCode } = await t.context.got.put(`user/${userName}/class/${className}`, {json: classroom});
             t.is(statusCode, 200);
         }
     }
@@ -91,16 +112,14 @@ test("PUT /user/{userName}/class/{className}", async (t) => {
 // Test endpoint for incorrect input
 test("Wrong input PUT /user/{userName}/class/{className}", async (t) => {
     // Test that missing className or userName throws
-    const requestBody = {
-        json: {}
-    }
+    const classroom = exampleClassObject
     const input_pairs = [
         ["", "someClassName"],
         ["someUserName", ""],
         ["", ""]
     ]
     for (const [userName, className] of input_pairs) {
-        const err = await t.throwsAsync(t.context.got.put(`user/${userName}/class/${className}`, requestBody))
+        const err = await t.throwsAsync(t.context.got.put(`user/${userName}/class/${className}`, {json: classroom}))
         t.is(err.response.statusCode, 404)
     }
 })
