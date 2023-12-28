b400a54fb5fbe58d24172ccb7944e62a445566ea
deepblue597 <iasonas2001@hotmail.com>
2023-12-28 17:31:08 +0200

    modified writer.js for McCabe Cyclomatic Complexity

--- utils/writer.js
+++ utils/writer.js
@@ -1,43 +1,27 @@
-var ResponsePayload = function(code, payload) {
-  this.code = code;
-  this.payload = payload;
+class ResponsePayload {
+	constructor(code, payload) {
+		this.code = code || 200;
+		this.payload = payload;
+	}
 }
 
-exports.respondWithCode = function(code, payload) {
-  return new ResponsePayload(code, payload);
-}
+exports.respondWithCode = function (code, payload) {
+	return new ResponsePayload(code, payload);
+};
 
-var writeJson = exports.writeJson = function(response, arg1, arg2) {
-  var code;
-  var payload;
+exports.writeJson = function (response, arg1, arg2) {
+	let code =
+		arg2 && Number.isInteger(arg2)
+			? arg2
+			: arg1 && Number.isInteger(arg1)
+			? arg1
+			: 200;
+	let payload = arg1 instanceof ResponsePayload ? arg1.payload : arg1;
 
-  if(arg1 && arg1 instanceof ResponsePayload) {
-    writeJson(response, arg1.payload, arg1.code);
-    return;
-  }
+	if (typeof payload === "object") {
+		payload = JSON.stringify(payload, null, 2);
+	}
 
-  if(arg2 && Number.isInteger(arg2)) {
-    code = arg2;
-  }
-  else {
-    if(arg1 && Number.isInteger(arg1)) {
-      code = arg1;
-    }
-  }
-  if(code && arg1) {
-    payload = arg1;
-  }
-  else if(arg1) {
-    payload = arg1;
-  }
-
-  if(!code) {
-    // if no response code given, we default to 200
-    code = 200;
-  }
-  if(typeof payload === 'object') {
-    payload = JSON.stringify(payload, null, 2);
-  }
-  response.writeHead(code, {'Content-Type': 'application/json'});
-  response.end(payload);
-}
+	response.writeHead(code, { "Content-Type": "application/json" });
+	response.end(payload);
+};
