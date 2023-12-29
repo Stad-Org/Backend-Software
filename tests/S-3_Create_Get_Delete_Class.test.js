4ddee6f1c50698ba16e3ff840ec521317f53b114
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2023-12-29 15:55:47 +0200

    Remove example tests & minor fixes

--- tests/S-3_Create_Get_Delete_Class.test.js
+++ tests/S-3_Create_Get_Delete_Class.test.js
@@ -7,10 +7,10 @@ const got = require("got");
 const onlyFunc = require('../service/DefaultService.js');
 
 // Import utils from S-3_utils.js
-const { testValidClass} = require('./S-3_utils.js');
+const { testValidClass } = require('./S-3_utils.js');
 
 // Import exampleClassObjects and expectedClassObject from types/types.js
-const { exampleClassObjects, expectedClassObject } = require('example_objects.js');
+const { exampleClassObjects, expectedClassObject } = require('./example_objects.js');
 
 // Initialize server
 test.before(async (t) => {
